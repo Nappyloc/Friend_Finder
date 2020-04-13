a3328ba7c395fbaa3acf1f335bf1869c0fe4090f
Dania King <dania@fortunebuilders.com>
2020-04-12 17:23:42 -0700

    updated questions

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,2 @@
+// Create a GET route with urel /api/friends (this will be used to dsoplay a JSON of possible friends)
+// Create a POST route to same database to hande incoming survey results. (also include the compatability logic)
---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,2 @@
+// Create Route to survey wich should display the survey page
+// Create a default route that leads to the home page
